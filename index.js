43e695e082cdc621569dad5a9809d04c9ba482f7
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-14 18:54:17 -0500

    feat: create setInterval for slideshow of images.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const header = document.getElementsByTagName('header')[0];
+const images = document.querySelectorAll('.carousel-image');
+let  currentImageIndex = 0; 
 
+setInterval(() => {
+  console.log('WORKS');
+
+  images[currentImageIndex].classList.add('hidden');
+
+  currentImageIndex++;
+  
+  if(currentImageIndex > 3) {
+    currentImageIndex = 0;
+  }
+
+  images[currentImageIndex].classList.remove('hidden');
+}, 5000);
 
 window.addEventListener('scroll', () => {
   // check the window object scrollY prop for when it is greater than equal to 50.
