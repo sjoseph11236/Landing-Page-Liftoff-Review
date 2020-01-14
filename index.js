1f43f50352d8e9efa609a6c7e56bafc160a2cb61
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-14 17:41:10 -0500

    chore: add opacity to the header

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const header = document.getElementsByTagName('header')[0];
 
 
 window.addEventListener('scroll', () => {
- 
-  console.log("TCL: scrollY ", scrollY);
+  // check the window object scrollY prop for when it is greater than equal to 50.
   if(window.scrollY >= 50) {
+    // toggle class to add active.
     header.classList.add('active');
   }
   else { 
+    // toggle class to remove active if scrollY is less than 50. 
     header.classList.remove('active');
   }
 });
