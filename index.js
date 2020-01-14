9fe09927acc6b0150da357e63d9a3a2d0ab0070e
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-14 17:28:37 -0500

    feat: add window event listener for scolling and changing header position Y

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const header = document.getElementsByTagName('header')[0];
+
+
+window.addEventListener('scroll', () => {
+ 
+  console.log("TCL: scrollY ", scrollY);
+  if(window.scrollY >= 50) {
+    header.classList.add('active');
+  }
+  else { 
+    header.classList.remove('active');
+  }
+});
